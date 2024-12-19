72f2966af921be8895ca089ce40ffe6dc52730d0
Raavi Kaur <raavi211299@gmail.com>
2024-12-19 11:13:48 -0500

    add comment to our code

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,12 +1,17 @@
+# compare and swap two adjacent elements until they are unsorted
+
 def bubble_sort(array):
-    for i in range(len(array)):
-        swapped = False
+    for i in range(len(array)):  # loop through each element of array
+        swapped = False  # keep track of swapping
         for j in range(len(array)-i-1):
-            if array[j] > array[j+1]:
+            if array[j] > array[j+1]:  # compare two adjacent elements
                 array[j], array[j+1] = array[j+1], array[j]
                 swapped = True
-        if not swapped:
+        if not swapped:  # means the array is already sorted
             break
+
 unsorted_array = [5, 9, 2, 1, 67, 34, 88, 34]
+# unsorted_array = [1, 2, 3, 4, 2]
+# unsorted_array = ["mona", "dhaval", "aamir", "tina", "chang"]
 bubble_sort(unsorted_array)
 print(unsorted_array)
