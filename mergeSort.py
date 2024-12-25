ab197b884d7f96c175bc0cd1d57f3668e80cf438
Raavi Kaur <raavi211299@gmail.com>
2024-12-25 15:01:00 -0500

    add comment to the file

--- mergeSort.py
+++ mergeSort.py
@@ -1,13 +1,15 @@
+# dividing a problem into multiple sub-problems. Each sub-problem is then solved individually. Finally, sub-problems
+# are combined to form the final solution
 
 def mergeSort(array):
     if len(array) > 1:
-        median = len(array)//2
+        median = len(array)//2  # median is the pt the array is divided into two sub arrays
         left = array[:median]
         right = array[median:]
-        mergeSort(left)
-        mergeSort(right)
-        i = j = k = 0
-        while i < len(left) and j < len(right):
+        mergeSort(left)  # sort the left half
+        mergeSort(right)  # sort the right half
+        i = j = k = 0  # i is index for left, j for right, k index for sorted array
+        while i < len(left) and j < len(right):  # loop until we reach end of either left or right
             if left[i] < right[j]:
                 array[k] = left[i]
                 i += 1
@@ -15,7 +17,7 @@ def mergeSort(array):
                 array[k] = right[j]
                 j += 1
             k += 1
-        while i < len(left):
+        while i < len(left):  # pickup the remaining elements and put in array
             array[k] = left[i]
             i += 1
             k += 1
@@ -24,7 +26,7 @@ def mergeSort(array):
             j += 1
             k += 1
 
-def display(array):
+def display(array):  # different way to display array
     for i in range(len(array)):
         print(array[i], end=" ")
     print()
