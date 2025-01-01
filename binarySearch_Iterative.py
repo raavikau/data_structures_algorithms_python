0ade1b46bb5a7657dfa87bcff679d5c5db399804
Raavi Kaur <raavi211299@gmail.com>
2025-01-01 18:00:46 -0500

    add code to search a value from the sorted list with iterative method

---
+++ binarySearch_Iterative.py
@@ -0,0 +1,21 @@
+def binary_search(number, target):
+    first = 0
+    last = len(number) -1
+    while first <= last:
+        median = (first + last) // 2
+        if number[median] == target:
+            return median
+        elif number[median] < target:
+            first = median + 1
+        else:
+            last = median - 1
+    return None
+def display(index):
+    if index is not None:
+        print("Target is at ", index)
+    else:
+        print("Target value is not find")
+
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = binary_search(data, 8)
+display(result)
